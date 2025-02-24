cbb67136730fa520c62c4acf33f49e3e2c200007
Vishnuprasath63 <145005255+Vishnuprasath63@users.noreply.github.com>
2025-02-24 20:16:38 +0530

    Updates

---
+++ precache-manifest.a6fb9578f267f2d76a317dab25876c45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55174cc0f53546eab23d90bd5a54da44",
+    "url": "/Vishnuprasath63/todo_web_app/index.html"
+  },
+  {
+    "revision": "fb6e9746269dff4e4b77",
+    "url": "/Vishnuprasath63/todo_web_app/static/css/2.28f15982.chunk.css"
+  },
+  {
+    "revision": "5dc44df2bb0429d08576",
+    "url": "/Vishnuprasath63/todo_web_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb6e9746269dff4e4b77",
+    "url": "/Vishnuprasath63/todo_web_app/static/js/2.e15c29e1.chunk.js"
+  },
+  {
+    "revision": "b14cf38e36b679bf3c7c",
+    "url": "/Vishnuprasath63/todo_web_app/static/js/3.bb0829b5.chunk.js"
+  },
+  {
+    "revision": "5dc44df2bb0429d08576",
+    "url": "/Vishnuprasath63/todo_web_app/static/js/main.97fbe340.chunk.js"
+  },
+  {
+    "revision": "c6fab30eb2a6ae0aa517",
+    "url": "/Vishnuprasath63/todo_web_app/static/js/runtime~main.29dccb03.js"
+  }
+]);
